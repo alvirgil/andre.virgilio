ccd64ab5dc614aaea3dc2ecc7a6df7eb6f799152
André Virgilio <andre.virgilio@kaspper.com.br>
2024-11-19 17:28:34 -0300

    removido backup

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
 var swiper = new Swiper(".right .slide-depoimentos", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
-  
-
-  var swiper = new Swiper(".carrossel", {
-    slidesPerView: "3",
-    spaceBetween: 24,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+var swiper = new Swiper(".carrossel", {
+  slidesPerView: "3",
+  spaceBetween: 24,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
