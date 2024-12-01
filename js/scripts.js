f2d472894d6cb4c6d04a80d1061fcb437c5de9ae
André Virgilio <andre.virgilio@kaspper.com.br>
2024-12-01 20:26:50 -0300

    Ajustes de todos os responsivos ate sessão CALL TO ACTION

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,42 @@ var swiper = new Swiper(".right .slide-depoimentos", {
   },
 });
 
-var swiper = new Swiper(".carrossel", {
-  slidesPerView: "3",
+var swiper = new Swiper(".s-portfolio .cases .carrossel", {
+  slidesPerView: 3, // Remover as aspas para garantir que seja um número
   spaceBetween: 24,
   autoplay: {
     delay: 2000,
     disableOnInteraction: true,
   },
   pagination: {
-    el: ".swiper-pagination",
+    el: ".cases .swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    1920: {
+      slidesPerView: 3, 
+    },
+    1440: {
+      slidesPerView: 3, 
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+    1150: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 2.3,
+    },
+    600: {
+      slidesPerView: 2, 
+    },
+    440: {
+      slidesPerView: 1.3, 
+    },
+    375: {
+      slidesPerView: 1,
+  },
+}
 });
+
